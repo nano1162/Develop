de0da430986551b701d3754e735b17a8c4f1f184
강동혁 <flynano1162@gmail.com>
2023-03-16 20:20:03 +0900

    대부분의 화면 기초 구현 완료.

--- ClassMemo/app.py
+++ ClassMemo/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,7 +8,31 @@ def index():
 
 @app.route('/login')
 def login():
-    return render_template("login.html")
+    if request.method == "GET":
+        return render_template("login.html")
+    # elif request.method == "POST":
+        
+
+@app.route('/register')
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    ''' elif request.method == "POST":
+
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        re_password = request.POST.get('re-password', None)
+
+        res_data = {}
+
+        if not (username and password and re_password):
+            res_data['error'] = '모든 값을 입력하세요.'
+        elif password != re_password :
+            res_data['error'] = '비밀번호가 다릅니다.'
+        else:
+            print('가입 완료! 아이디와 비밀번호는 저장되지 않았습니다 - Test Build')
+
+        return render_template('login.html') '''
 
 @app.route('/science')
 def science():
